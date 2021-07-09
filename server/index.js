5bbb988448f1b3385a811ac0b41997b81c617ade
Gabriel Caian <gabrielcaiangc@gmail.com>
2021-07-09 17:59:21 -0300

    Configurações iniciais do server

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const app = require('./config/express-config');
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World<h1>');
+});
+
+app.get('/user', (req, res) => {
+  const pessoa = {
+    nome: 'Gabriel',
+    sobrenome: 'Cabral',
+  };
+
+  res.json(pessoa);
+});
+
+app.post('/mirror', (req, res) => {
+  res.json(req.body);
+});
+
+app.listen(3000, 'localhost', () => console.log('Servidor Rodando'));
